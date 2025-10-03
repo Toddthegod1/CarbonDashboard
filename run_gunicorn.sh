d888dff560d4193a1f86a7a73215a8fc5523e187
Toddthegod1 <toddjek@icloud.com>
2025-10-03 21:10:37 +1300

    updating gunicorn

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 export $(grep -v '^#' .env | xargs)
-exec gunicorn -w 3 -b 0.0.0.0:8000 "app:create_app()"
+ExecStart=/usr/bin/python3 /usr/local/bin/gunicorn -w 3 -b 0.0.0.0:8000 "app:create_app()"
